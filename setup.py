709924361a09409f6193548d040bcd2a55ba52f0
Luke Du <sinnud@gmail.com>
2021-01-02 11:01:47 -0800

    correct folder structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="example-pkg-sinnud", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Luke Du",
     author_email="sinnud@gmail.com",
     description="A small example package",
